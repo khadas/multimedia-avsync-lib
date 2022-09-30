a23122cf27cfd19abe2e0a24afa277e29ed1e5f0
Song Zhao <song.zhao@amlogic.com>
2022-09-30 16:35:54 -0700

    avsync-lib: update level micro for syslog and logcat [1/1]
    PD#TV-61176
    
    Problem:
    To avoid conflict from syslog
    
    Solution:
    Add extra prefix
    
    Verify:
    T5D + Roxton
    
    Signed-off-by: Song Zhao <song.zhao@amlogic.com>
    Change-Id: Ifc07c50517995a2e47acd6e6cd45791d2d091624

--- src/aml_avsync_log.h
+++ src/aml_avsync_log.h
@@ -15,14 +15,14 @@
 
 typedef void (*log_LockFn)(void *udata, int lock);
 
-enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
+enum { AVS_LOG_TRACE, AVS_LOG_DEBUG, AVS_LOG_INFO, AVS_LOG_WARN, AVS_LOG_ERROR, AVS_LOG_FATAL };
 
-#define log_trace(...) log_log(LOG_TRACE, __func__, __LINE__, __VA_ARGS__)
-#define log_debug(...) log_log(LOG_DEBUG, __func__, __LINE__, __VA_ARGS__)
-#define log_info(...)  log_log(LOG_INFO,  __func__, __LINE__, __VA_ARGS__)
-#define log_warn(...)  log_log(LOG_WARN,  __func__, __LINE__, __VA_ARGS__)
-#define log_error(...) log_log(LOG_ERROR, __func__, __LINE__, __VA_ARGS__)
-#define log_fatal(...) log_log(LOG_FATAL, __func__, __LINE__, __VA_ARGS__)
+#define log_trace(...) log_log(AVS_LOG_TRACE, __func__, __LINE__, __VA_ARGS__)
+#define log_debug(...) log_log(AVS_LOG_DEBUG, __func__, __LINE__, __VA_ARGS__)
+#define log_info(...)  log_log(AVS_LOG_INFO,  __func__, __LINE__, __VA_ARGS__)
+#define log_warn(...)  log_log(AVS_LOG_WARN,  __func__, __LINE__, __VA_ARGS__)
+#define log_error(...) log_log(AVS_LOG_ERROR, __func__, __LINE__, __VA_ARGS__)
+#define log_fatal(...) log_log(AVS_LOG_FATAL, __func__, __LINE__, __VA_ARGS__)
 
 void log_set_udata(void *udata);
 void log_set_lock(log_LockFn fn);
